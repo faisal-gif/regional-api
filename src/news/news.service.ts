8221a6c8b9f623f82a2d3fcaa3ff913eaba5faf6
faisal-gif <fagdera@gmail.com>
2026-02-09 20:31:07 +0700

    modified:   src/news/news.service.ts

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -362,14 +362,6 @@ export class NewsService {
     async findOne(code: string) {
         const cacheKey = `news_detail_${code}`;
 
-        // 1. Increment Views (Gunakan Raw SQL juga agar lebih enteng)
-        // Fire-and-forget (tanpa await) agar tidak memblokir response
-        const randomViews = Math.floor(Math.random() * 200) + 1;
-        this.repo.query(
-            `UPDATE news SET views = views + ? WHERE is_code = ?`,
-            [randomViews, code]
-        ).catch(err => console.error("Gagal update views:", err));
-
         // 2. Cek Cache
         const cachedData = await this.cacheManager.get<NewsDetailDto>(cacheKey);
         if (cachedData) return cachedData;
